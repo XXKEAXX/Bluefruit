74de0385a2c724d469e9e7e727c6e28ead9dae9e
XXKEAXX <42928906+XXKEAXX@users.noreply.github.com>
2018-09-11 12:52:35 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -82,7 +82,7 @@ function onConnect(){
 			if(rssi < 0)
 				rssi = (rssi * -1);
 			
-			if(rssi <= 48)
+			if(rssi <= 40)
 				alert("FOUND");
 				
 			document.getElementById("statusDiv").innerHTML = " Status: Connected: "+rssi;
