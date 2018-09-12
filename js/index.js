1f2ebce2aafd5cc4744a5da6e4634c8d5ad9c0ce
XXKEAXX <42928906+XXKEAXX@users.noreply.github.com>
2018-09-12 12:01:05 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -114,7 +114,7 @@ function onDeviceReady(){
 						 	 ble.writeWithoutResponse(deviceId, 
 						 	 blue.serviceUUID,
 				 		 	 blue.txCharacteristic, 
-				 			 stringToBytes("hello ble!\r\n\r\n"), function() {
+				 			 stringToBytes("hello ble!+--+"), function() {
 	
 	 
 							   BlockInterval = 1;
